c4d1f5883c6e66b32ba0213fe77757b5e727f3a5
shiva <bhattaraishiva17@gmail.com>
2017-02-14 08:31:34 +0200

    bootstrap and changes in vaious cases

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  resources :titles
+
   devise_for :users
-  root 'address_books#index'
+  devise_scope :user do
+    authenticated :user do
+      root 'address_books#index', as: :authenticated_root
+    end
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+  resources :titles
   resources :address_books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
