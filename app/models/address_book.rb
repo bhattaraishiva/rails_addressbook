9be13448284742eedffb7136542f626d10c2091c
shiva <bhattaraishiva17@gmail.com>
2017-02-16 23:38:48 +0200

    improvement in index

--- app/models/address_book.rb
+++ app/models/address_book.rb
@@ -14,19 +14,19 @@ class AddressBook < ApplicationRecord
     "#{self.title.title} #{self.first_name} #{self.last_name}"
   end
 
-def full_address
-  if @address_book.addresses.any?
-    @address_book.addresses.each do |a|
-    "#{a.street}, #{a.city}, #{ a.state}, #{a.zipcode}, #{a.country}"
+  def full_address
+    if @address_book.addresses.any?
+      @address_book.addresses.each do |a|
+        "#{a.street}, #{a.city}, #{ a.state}, #{a.zipcode}, #{a.country}"
+      end
     end
   end
-end
-def self.to_csv
-  attributes = %w{}
-  CSV.generate(header: true) do |csv|
-      csv << column_names
+  def self.to_csv
+  attributes = %w{id, full_name, addresses,, phone_numbers, date_of_birth}
+  CSV.generate(headers: true) do |csv|
+      csv << attributes
       all.each do |address_book|
-        csv << address_book.attributes.values_at(*column_names)
+        csv << address_book.attributes.values_at(*attributes)
       end
   end
 end
